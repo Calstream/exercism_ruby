dae9a4137f3f9dd7c47aa890ef3f7d86fa282f3e
Argos <calstream.lgn@gmail.com>
2018-02-20 18:59:59 +0300

    raindrops

---
+++ raindrops/raindrops.rb
@@ -0,0 +1,21 @@
+class Raindrops
+  def self.convert number
+    sound = ""
+    if number % 3 == 0
+      sound += "Pling"
+    end
+  
+    if number % 5 == 0
+      sound += "Plang"
+    end
+    
+    if number % 7 == 0
+      sound += "Plong"
+    end
+    
+    if sound == ""
+      return number.to_s
+    end
+    sound
+  end
+end
